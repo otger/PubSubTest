edd247ac178e069651e927d9c24128610bdfa768
otger <otger.ballester@gmail.com>
2017-01-04 20:43:28 +0100

    Implemented test to check delay between pub and sub

---
+++ tests/delay.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from pubsub.dealer.dealer import Dealer
+from modules.genmod import GenericModule
+import time
+from threading import Lock
+try:
+    import matplotlib.pyplot as plt
+    MATPLOTLIB = True
+except ImportError:
+    MATPLOTLIB = False
+try:
+    from scipy import stats
+    SCIPY = True
+except ImportError:
+    SCIPY = False
+
+__author__ = 'otger'
+
+
+class TValue(object):
+    def __init__(self, value):
+        self._init = time.time()
+        self.value = value
+
+    def _get_elapsed(self):
+        return time.time() - self._init
+    elapsed = property(_get_elapsed)
+
+    def reset(self):
+        self._init = time.time()
+
+
+class DelayModule(GenericModule):
+    def __init__(self, pair, count, pairid, lead=False):
+        self.pair = pair
+        self.lead_element = lead
+        self.elementid = 0 if lead else 1
+        self.partnerid = 1 if lead else 0
+        self.pairid = pairid
+        rootname = 'pair{0}.element{1}'.format(pairid, self.elementid)
+        GenericModule.__init__(self, dealer=pair.dealer, root_name=rootname)
+
+        self._count = count
+        self.init_count = count
+
+        self.pub_path = 'count'
+
+        self.partnername = 'pair{0}.element{1}'.format(pairid, self.partnerid)
+        self.partner_pub_path = '{0}.count'.format(self.partnername)
+
+        self.subscribe(self.cb, self.partner_pub_path)
+        self.delays = []
+        print('{0} subscribed to {1}'.format(self.name, self.partner_pub_path))
+
+        self._start = time.time()
+
+    def cb(self, pqv):
+        tvalue = pqv.value
+        self.delays.append(tvalue.elapsed)
+        if self.lead_element and tvalue.value <= 0:
+            print("Pair {0} elapsed {1} seconds to countdown from {2}".format(self.pairid, time.time()-self._start,
+                                                                              self.init_count))
+            self.pair.done()
+        else:
+            tvalue.value -= 1
+            tvalue.reset()
+            self.p(tvalue)
+
+    def start(self):
+        self._start = time.time()
+        self.p(TValue(self.init_count))
+
+    def p(self, value):
+        self.publish(self.pub_path, value)
+        # print("{0} published {1}: {2}".format(self.name, self.pub_path, value))
+
+
+class DelayPair(object):
+    def __init__(self, dealer, count, pairid):
+        self.dealer = dealer
+        self.pairid = pairid
+        self.lead = DelayModule(pair=self, count=count, pairid=pairid, lead=True)
+        self.foll = DelayModule(pair=self, count=count, pairid=pairid, lead=False)
+        self._l = Lock()
+
+    def start(self):
+        try:
+            self._l.acquire(blocking=False)
+        except Exception as ex:
+            print(ex)
+        self.lead.start()
+
+    def stop(self):
+        self.lead.exit()
+        self.foll.exit()
+
+    def done(self):
+        try:
+            self._l.release()
+        except Exception as ex:
+            print(ex)
+
+    def wait_done(self):
+        self._l.acquire(blocking=True)
+
+
+class Delay(object):
+    def __init__(self, count=1000, couples=1):
+        self.dealer = Dealer()
+        self.couples = []
+        for i in range(couples):
+            self.couples.append(DelayPair(dealer=self.dealer,
+                                          count=count,
+                                          pairid=i))
+
+    def start(self):
+        for el in self.couples:
+            el.start()
+
+    def stop(self):
+        for el in self.couples:
+            el.stop()
+
+    def wait_done(self):
+        for el in self.couples:
+            el.wait_done()
+
+if __name__ == "__main__":
+
+    c = Delay(count=5000, couples=5)
+    c.start()
+    # time.sleep(4)
+    # c.stop()
+    # c.dealer.close()
+    c.wait_done()
+    c.stop()
+    c.dealer.close()
+
+    for el in c.couples:
+        a = el.lead.delays + el.foll.delays
+    if MATPLOTLIB:
+        plt.hist(a, bins=30, alpha=0.5)
+        plt.xlabel("Delay")
+        plt.ylabel("Frequency")
+        # plt.legend()
+        plt.show()
+    if SCIPY:
+        print(stats.describe(a))
